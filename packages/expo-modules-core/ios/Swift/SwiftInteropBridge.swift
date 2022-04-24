3565c6a7966956a7a69e75ca8be39fceddf4e88d
Tomasz Sapeta <tsapeta@users.noreply.github.com>
2022-04-24 23:10:40 +0200

    [ios] Split the implementation of async and sync functions

--- packages/expo-modules-core/ios/Swift/SwiftInteropBridge.swift
+++ packages/expo-modules-core/ios/Swift/SwiftInteropBridge.swift
@@ -42,12 +42,11 @@ public final class SwiftInteropBridge: NSObject {
   ) {
     registry
       .get(moduleHolderForName: moduleName)?
-      .call(function: functionName, args: args) { value, error in
-        if let error = error {
-          reject(error.code, error.description, error)
-        } else if let error = error {
-          reject("ERR_UNKNOWN_ERROR", error.localizedDescription, error)
-        } else {
+      .call(function: functionName, args: args) { result in
+        switch result {
+        case .failure(let error):
+          reject(error.code, error.description, nil)
+        case .success(let value):
           resolve(value)
         }
       }
